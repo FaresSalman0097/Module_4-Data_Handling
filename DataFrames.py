3961a870580f87549a5eb640106b47584f65a8c9
Salman0097 <91413898+Salman0097@users.noreply.github.com>
2022-09-23 21:04:30 +0530

    sep 23

--- DataFrames.py
+++ DataFrames.py
@@ -5,10 +5,19 @@ data = {
     "Age": [29, 27]
 }
 df = pd.DataFrame(data)
-# print(df)
+#print(df)
 # print(df.loc[0])
-print(df.loc[1, 1])
-# print(df["Name"])
+#print(df.loc[[1, 1]])
+#print(df["Name"])
+
+# named indexes
+data1 = {
+    "Name": ["Vijay", "Vikram"],
+    "Age": [48, 54]
+}
+df1 = pd.DataFrame(data1, index=["Varisu","Cobra"])
+print(df1)
+
 
 
 
--- PandasSeries.py
+++ PandasSeries.py
@@ -12,3 +12,14 @@ ser1=pd.Series(list1,index=["First","Second",103,"Fourth",105,"Last"])
 print(ser1)
 print(ser1['Second'])
 print(ser1[105])
+
+# Key/value as series
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar1 = pd.Series(calories)
+print(myvar1)
+
+# indexing key/value pair
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar2 = pd.Series(calories, index = ["day1", "day2"])
+print(myvar2)
